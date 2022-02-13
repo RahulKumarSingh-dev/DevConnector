e14c88f864ad37bf32d450389a6eece17571bb16
Rahul Kumar <rahulkumar020702@gmail.com>
2022-02-13 14:42:39 +0530

    Prepared for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
-module.exports={
-  mongoURI:'mongodb+srv://Rahul:rahul@devconnector.nlr2i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
